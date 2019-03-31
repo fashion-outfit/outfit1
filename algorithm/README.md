cef91ebdfa3d41e17e85bc218d46bfc2b1293fff
Robert Holzmann <leo_wsy@outlook.com>
2019-03-31 22:48:15 +0800

    Skeleton of network + gitignore simplified

---
+++ algorithm/README.md
@@ -0,0 +1,2 @@
+# Project Codename 'outfit1' - Alogirthm
+A reoccurance of [Interpretable Partitioned Embedding for Customized Fashion Outfit Composition](https://arxiv.org/abs/1806.04845)
---
+++ algorithm/network.py
@@ -0,0 +1,15 @@
+import numpy as np
+import tensorflow as tf
+import tflearn as tfl
+
+def ENet(x, reuse=False):
+    pass
+
+def PNet(x, reuse=False):
+    pass
+
+def ANet(x, reuse=False):
+    pass
+
+def DNet(x, reuse=False):
+    pass
