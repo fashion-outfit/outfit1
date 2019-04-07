91369526e73bbcf3b6a2e7409f9dc873c00c25fd
aoru45 <479192742@qq.com>
2019-04-07 12:13:04 +0800

    upload frontenf code

---
+++ frontend/pages/index/index.js
@@ -0,0 +1,34 @@
+//index.js
+//获取应用实例
+const app = getApp()
+Page({
+  data: {
+    gridArray:
+      [
+        {
+          "img": "../../images/bd.png",
+          "name": "百搭"
+        },
+        {
+          "img": "../../images/rl.png",
+          "name": "瑞丽"
+        },
+        {
+          "img": "../../images/xy.png",
+          "name": "学院"
+        },
+        {
+          "img": "../../images/hb.png",
+          "name": "韩版"
+        },
+        {
+          "img": "../../images/mz.png",
+          "name": "民族"
+        },
+        {
+          "img": "../../images/more.png",
+          "name": "更多"
+        },
+      ]
+  }
+})
